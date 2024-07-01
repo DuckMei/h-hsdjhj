004015aaaa9b377cfe03ba9e3ccf7be7e9f42eb0
Duke <94008471+DuckMei@users.noreply.github.com>
2024-07-01 12:27:19 +0200

    Update connect.js

--- Databases/connect.js
+++ Databases/connect.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 
 async function connect() {
   await mongoose.connect(process.env.MongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+			useNewUrlParser: true,
+			autoIndex: false,
+			connectTimeoutMS: 10000,
+			useUnifiedTopology: true,
+		})
   const db = mongoose.connection;
   db.on("error", console.error.bind(console, "connection error: "));
-  db.once("open", function() {
+  db.once("connected", function() {
     console.log('✅ Đã Kết Nối Tới Database'.bold.brightBlue)
   });
   return;
